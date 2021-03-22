b45d1c38a5999b575f12e3ed5e084ade0c5b5734
ViperSLM <crisjstow@gmail.com>
2021-03-22 17:49:38 +1030

    Forgot to add comments to the script
    Whoops!

--- finddll.sh
+++ finddll.sh
@@ -3,12 +3,12 @@
 
 FILE=$1
 
-if [ -f "${FILE}" ]; then
+if [ -f "${FILE}" ]; then # Check if the specified executable exists in the user's current directory before proceeding
 	x86_64-w64-mingw32-objdump -p ${FILE} | grep 'DLL Name:' | sed -e "s/\t*DLL Name: //g"
 else
-	if [ -z "$1" ]; then
+	if [ -z "$1" ]; then # If no argument is made (e.g. No .exe file specified, display the usage message
 		printf "\nFindDLL - Usage:\nfinddll [Windows executable]\n\n"
-	else
+	else # Otherwise, tell the user that the file specified does not exist
 		echo "The file ${FILE} does not exist in the current directory."
 	fi
 fi
